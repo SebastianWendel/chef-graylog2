8d28dd099b948dbd8b0dfbaeb534550e3680b7d8
Sebastian Wendel <s.wendel@sourceindex.de>
2012-04-28 16:52:28 +0200

    added config default config variables

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,45 +18,55 @@
 # limitations under the License.
 #
 
-# project download site
+# PROJECT SITE
 default['graylog2']['project_url'] = "https://github.com/downloads/Graylog2"
 
-# server binary
+# SERVER BINARY
 default['graylog2']['server_version'] = "0.9.6p1-RC1"
 default['graylog2']['server_file'] = "graylog2-server-#{node['graylog2']['server_version']}.tar.gz"
 default['graylog2']['server_download'] = "#{node['graylog2']['project_url']}/graylog2-server/#{node['graylog2']['server_file']}"
 default['graylog2']['server_checksum'] = "8bdddfc2ba9b8537f705f997461bd40d3a4091cd3f2b824622704a62ef1c0b96"
 
-# web binary
+# WEB BINARY
 default['graylog2']['web_version'] = "0.9.6p1-RC1"
 default['graylog2']['web_file'] = "graylog2-web-interface-#{node['graylog2']['web_version']}.tar.gz"
 default['graylog2']['web_download'] = "#{node['graylog2']['project_url']}/graylog2-web-interface/#{node['graylog2']['web_file']}"
 default['graylog2']['web_checksum'] = "b2f8951a7effc1c3b617482bea0c79427f801f4034525adb163d041c34707fc1"
 
-# syslog4j binary
+# SYSLOG4J BINARY
 default['graylog2']['syslog4j_version'] = "0.9.46"
 default['graylog2']['syslog4j_file'] = "syslog4j-#{node['graylog2']['syslog4j_version']}-bin.jar"
 default['graylog2']['syslog4j_download'] = "http://syslog4j.org/downloads/#{node['graylog2']['syslog4j_file']}"
 default['graylog2']['syslog4j_checksum'] = "9c9f44fb457fc3157f5dab7e9c94b027e964986545d8ee312380ae3f882a75ed"
 
-# server config
+# SERVER CONFIG
 default['graylog2']['server_path'] = "/usr/share/graylog2-server"
 default['graylog2']['server_user'] = "graylog2-server"
 default['graylog2']['server_group'] = "graylog2-server"
 default['graylog2']['server_storage'] = "elasticsearch"
 default['graylog2']['server_port'] = 5140
 
-# syslog4j config
+# SYSLOG4J CONFIG
 default['graylog2']['syslog4j_path'] = "/usr/share/syslog4j"
 
-# webfrontend config
+# WEB FRONTEND CONFIG
 default['graylog2']['ruby_version'] = "ruby-1.9.3-p125"
 default['graylog2']['passenger_version'] = "3.0.12"
 default['graylog2']['web_path'] = "/home/graylog2-web" # note that rvm doesn't take any different
 default['graylog2']['web_user'] = "graylog2-web"
 default['graylog2']['web_group'] = "graylog2-web"
 
-# mongodb config
+# EMAIL HOST CONFIG
+default['graylog2']['email_type'] = "smtp"
+default['graylog2']['email_host'] = "127.0.0.1"
+default['graylog2']['email_tls'] = "true"
+default['graylog2']['email_port'] = "25"
+default['graylog2']['email_auth'] = "plain" # plain, login, cram_md5 - Comment out or remove to use no auth                         
+default['graylog2']['email_user'] = nil
+default['graylog2']['email_passwd'] = nil
+default['graylog2']['email_domain'] = "#{node['fqdn']}"
+
+# MONGODB CONFIG
 default['graylog2']['mongo_host'] = "127.0.0.1"
 default['graylog2']['mongo_port'] = 27017
 default['graylog2']['mongo_maxconnections'] = 150
@@ -66,7 +76,12 @@ default['graylog2']['mongo_user'] = nil
 default['graylog2']['mongo_passwd'] = nil
 default['graylog2']['mongo_replica'] = "localhost:27017,localhost:27018,localhost:27019"
 
-# notification config
+# ELASTICSEARCH CONFIG
+default['graylog2']['elastic_host'] = "127.0.0.1"
+default['graylog2']['elastic_port'] = 9200
+default['graylog2']['elastic_index'] = "graylog2"
+
+# NOTIFI CRONS CONFIG
 default['graylog2']['send_stream_alarms'] = true
 default['graylog2']['send_stream_subscriptions'] = true
 default['graylog2']['stream_alarms_cron_minute'] = "*/15"
