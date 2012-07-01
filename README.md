6cb8498125ad21fb5918c71a647a4147548023e4
Sebastian Wendel <s.wendel@sourceindex.de>
2012-07-01 20:03:33 +0200

    [FIX] refactoring of all variables

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Description #
 
+A cookbook for Opscodes Chef open-source systems integration framework to install and configure the Graylog2 Server and Web-Interface.
+
+Graylog2 is an open source log management solution that stores your logs in ElasticSearch. It consists of a server written in Java that accepts your syslog messages via TCP, UDP or AMQP and stores it in the database. The second part is a web interface that allows you to manage the log messages from your web browser.
+
 http://www.graylog2.org
 
 # Requirements #
@@ -25,11 +29,11 @@ To install the coockbook use the following commands, depending to your platform:
     cookbook 'apt', :git => 'https://github.com/opscode-cookbooks/apt.git'
     cookbook 'yum', :git => 'https://github.com/opscode-cookbooks/yum.git'
     cookbook 'java', :git => 'https://github.com/opscode-cookbooks/java.git'
-    cookbook 'graylog2', :git => 'https://github.com/sebwendel/chef-graylog2.git'
     cookbook 'elasticsearch', :git => 'https://github.com/sebwendel/chef-elasticsearch.git'
     cookbook 'mongodb', :git => 'https://github.com/sebwendel/chef-mongodb.git'
     cookbook 'rvm', :git => 'https://github.com/fnichol/chef-rvm.git'
     cookbook 'apache2', :git => 'https://github.com/opscode-cookbooks/apache2.git'
+    cookbook 'graylog2', :git => 'https://github.com/sebwendel/chef-graylog2.git'
     END_OF_CHEFFILE
 
     librarian-chef install
@@ -51,11 +55,26 @@ This will install the java dependencie, the graylog2 server and the declared plu
 * `node['graylog2']['interface']` - "127.0.0.1" limit to local access, default is "0.0.0.0".
 * `node['graylog2']['port']` - database listener port, default is "27017".
 
+# Usage #
+1. download and install all dependencies
+1. override the attributes if necessary
+1. check all interface, port and authentication config
+1. add the recipe to your nodes runlist 
+
+    {
+      "run_list": [
+        "recipe[graylog2]"
+      ]
+    }
+
+If you use all dependencies from the described sources the cookbook should work out of the box without any changes of attributes.
+
 # Limitations #
-At the moment the cookbook just contains a single node procedure.
+* For now there is no authentication on the backend side.
 
 # ToDos and Issues #
-Have a lock at the github issues section. There's still some work to do, patches are more than welcome.
+If you have any questions or recommendations just create a issue at the github repository.
+If you want to help have a lock at the github issues section, patches are more than welcome.
 
 # License and Author #
 
--- recipes/web-interface.rb
+++ recipes/web-interface.rb
@@ -17,18 +17,6 @@
 # limitations under the License.
 #
 
-# LOCAL VARIABLES
-RUBY_VERSION      = node['GRAYLOG2']['RUBY_VERSION']
-PASSENGER_VERSION = node['GRAYLOG2']['PASSENGER_VERSION']
-WEB_PATH          = node['GRAYLOG2']['WEB_PATH']
-WEB_USER          = node['GRAYLOG2']['WEB_USER']
-WEB_GROUP         = node['GRAYLOG2']['WEB_GROUP']
-WEB_DOWNLOAD      = node['GRAYLOG2']['WEB_DOWNLOAD']
-WEB_VERSION       = node['GRAYLOG2']['WEB_VERSION']
-WEB_FILE          = node['GRAYLOG2']['WEB_FILE']
-WEB_CHECKSUM      = node['GRAYLOG2']['WEB_CHECKSUM']
-
-# COOKBOOK DEPENDENCIES
 case node['platform']
 when "debian", "ubuntu"
   include_recipe 'apt'
@@ -37,171 +25,146 @@ when "centos","redhat"
 else
     Chef::Log.warn("The #{node['platform']} is not yet not supported by this cookbook")
 end
+
 include_recipe "apache2"
 include_recipe "apache2::mod_ssl"
 include_recipe "apache2::mod_rewrite"
 
-# PACKAGE DEPENDENCIES
 package "postfix"
 package "apache2-dev"
 package "libcurl4-openssl-dev"
 
-# ADD GRAYLOG2 APACHE CONFIG
 template "Added Graylog2 Web-Interface Apache config." do
   path "/etc/apache2/sites-available/graylog2"
   source "apache2.erb"
   mode 0644
 end
 
-# DISABLE DEFAULT APACHE SITE
 apache_site "000-default" do
   enable false
 end
 
-# ENABLE GRAYLOG2 APACHE SITE
 apache_site "graylog2"
 
-# CREATE GROUP
-group WEB_GROUP do
+group node['graylog2']['web_group'] do
   system true
 end
 
-# CREATE USER
-user WEB_USER do
-  home WEB_PATH
-  gid WEB_GROUP
+user node['graylog2']['web_user'] do
+  home node['graylog2']['web_path']
+  gid node['graylog2']['web_group']
   comment "services user for thr graylog2-web-interface"
   supports :manage_home => true
   shell "/bin/bash"
 end
 
-# INSTALL SOURCE FILE IF NOT EXISTS
-unless FileTest.exists?("#{WEB_PATH}/graylog2-web-interface-#{WEB_VERSION}")
-  remote_file "#{Chef::Config[:file_cache_path]}/#{WEB_FILE}" do
-    source WEB_DOWNLOAD
-    checksum WEB_CHECKSUM
+unless FileTest.exists?("#{node['graylog2']['web_path']}/graylog2-web-interface-#{node['graylog2']['web_version']}")
+  remote_file "#{Chef::Config[:file_cache_path]}/#{node['graylog2']['web_file']}" do
+    source node['graylog2']['web_download']
+    checksum node['graylog2']['web_checksum']
     action :create_if_missing
   end
 
-  bash "install graylog2 sources #{WEB_FILE}" do
+  bash "install graylog2 sources #{node['graylog2']['web_file']}" do
     cwd Chef::Config[:file_cache_path]
     code <<-EOH
-      tar -zxf #{WEB_FILE} -C #{WEB_PATH}
+      tar -zxf #{node['graylog2']['web_file']} -C #{node['graylog2']['web_path']}
     EOH
   end
 
-  link "#{WEB_PATH}/current" do
-    to "#{WEB_PATH}/graylog2-web-interface-#{WEB_VERSION}"
+  link "#{node['graylog2']['web_path']}/current" do
+    to "#{node['graylog2']['web_path']}/graylog2-web-interface-#{node['graylog2']['web_version']}"
   end
 
-  log "Downloaded, installed and configured the Graylog2 Web binary files in #{WEB_PATH}/#{WEB_VERSION}." do
+  log "Downloaded, installed and configured the Graylog2 Web binary files in #{node['graylog2']['web_path']}/#{node['graylog2']['web_version']}." do
     action :nothing
   end
 end
 
-# CREATE GENERAL CONFIG-FILE
 template "Create graylog2-web general config." do
-  path "#{WEB_PATH}/current/config/general.yml"
+  path "#{node['graylog2']['web_path']}/current/config/general.yml"
   source "general.yml.erb"
-  owner WEB_USER
-  group WEB_GROUP
+  owner node['graylog2']['web_user']
+  group node['graylog2']['web_group']
   mode 0644
 end
 
-# CREATE MONGODB CONFIG-FILE
 template "Create graylog2-web mongodb config." do
-  path "#{WEB_PATH}/current/config/mongoid.yml"
+  path "#{node['graylog2']['web_path']}/current/config/mongoid.yml"
   source "mongoid.yml.erb"
-  owner WEB_USER
-  group WEB_GROUP
+  owner node['graylog2']['web_user']
+  group node['graylog2']['web_group']
   mode 0644
 end
 
-# CREATE ELASTICSEARCH CONFIG-FILE
 template "Create graylog2-web indexer config." do
-  path "#{WEB_PATH}/current/config/indexer.yml"
+  path "#{node['graylog2']['web_path']}/current/config/indexer.yml"
   source "indexer.yml.erb"
-  owner WEB_USER
-  group WEB_GROUP
+  owner node['graylog2']['web_user']
+  group node['graylog2']['web_group']
   mode 0644
 end
 
-# CREATE EMAIL-SERVER CONFIG-FILE
 template "Create graylog2-web email config." do
-  path "#{WEB_PATH}/current/config/email.yml"
+  path "#{node['graylog2']['web_path']}/current/config/email.yml"
   source "email.yml.erb"
-  owner WEB_USER
-  group WEB_GROUP
+  owner node['graylog2']['web_user']
+  group node['graylog2']['web_group']
   mode 0644
 end
 
-# INSTALL USER ISOLATED RVM
 node['rvm']['user_installs'] = [
-  { 'user' => WEB_USER }
+  { 'user' => node['graylog2']['web_user'] }
 ]
 
 include_recipe "rvm::user_install"
 
-# INSTALL USER ISOLATED RUBY
-rvm_ruby RUBY_VERSION do
-  user WEB_USER
+rvm_ruby node['graylog2']['ruby_version'] do
+  user node['graylog2']['web_user']
 end
 
-# INSTALL BUNDLER GEM
 rvm_gem "bundler" do
-  user WEB_USER
+  user node['graylog2']['web_user']
 end
 
-# INSTALL PASSENGER GEM
 rvm_gem "passenger" do
-  user WEB_USER
-  version PASSENGER_VERSION
+  user node['graylog2']['web_user']
+  version node['graylog2']['passenger_version']
 end
 
-# TAKE OWNERSHIP OF EVERTHING
 execute "graylog2-web-interface owner-change" do
-    command "chown -Rf #{WEB_USER}:#{WEB_GROUP} #{WEB_PATH}"
+    command "chown -Rf #{node['graylog2']['web_user']}:#{node['graylog2']['web_group']} #{node['graylog2']['web_path']}"
 end
 
-# INSTALL APACHE PASSENGER MODUL IF NOT EXISTS
 rvm_shell "passenger module install" do
-  user WEB_USER
-  group WEB_GROUP
-  creates "#{WEB_PATH}/.rvm/gems/#{RUBY_VERSION}/gems/passenger-#{PASSENGER_VERSION}/ext/apache2/mod_passenger.so"
-  cwd WEB_PATH
+  user node['graylog2']['web_user']
+  group node['graylog2']['web_group']
+  creates "#{node['graylog2']['web_path']}/.rvm/gems/#{node['graylog2']['ruby_version']}/gems/passenger-#{node['graylog2']['passenger_version']}/ext/apache2/mod_passenger.so"
+  cwd node['graylog2']['web_path']
   code %{passenger-install-apache2-module --auto}
 end
 
-# INSTALL ALL GEM DEPENDENCIES
 rvm_shell "run bundler install" do
-  user WEB_USER
-  group WEB_GROUP
-  cwd "#{WEB_PATH}/current"
+  user node['graylog2']['web_user']
+  group node['graylog2']['web_group']
+  cwd "#{node['graylog2']['web_path']}/current"
   code %{bundle install}
 end
 
-# ADD STREAM ALARM CRON FOR THE USER
 cron "Graylog2 send stream alarms" do
-  user WEB_USER
-  minute node['GRAYLOG2']['STREAM_ALARMS_CRON_MINUTE']
-  action node['GRAYLOG2']['SEND_STREAM_ALARMS'] ? :create : :delete
-  command "source ~/.bashrc && cd #{WEB_PATH}/current && RAILS_ENV=production rake streamalarms:send"
+  user node['graylog2']['web_user']
+  minute node['graylog2']['stream_alarms_cron_minute']
+  action node['graylog2']['send_stream_alarms'] ? :create : :delete
+  command "source ~/.bashrc && cd #{node['graylog2']['web_path']}/current && RAILS_ENV=production rake streamalarms:send"
 end
 
-# ADD STREAM SUBSCRIPTION CRON FOR THE USER
 cron "Graylog2 send stream subscriptions" do
-  user WEB_USER
-  minute node['GRAYLOG2']['STREAM_SUBSCRIPTIONS_CRON_MINUTE']
-  action node['GRAYLOG2']['SEND_STREAM_SUBSCRIPTIONS'] ? :create : :delete
-  command "source ~/.bashrc && cd #{WEB_PATH}/current && RAILS_ENV=production rake subscriptions:send"
+  user node['graylog2']['web_user']
+  minute node['graylog2']['stream_subscriptions_cron_minute']
+  action node['graylog2']['send_stream_subscriptions'] ? :create : :delete
+  command "source ~/.bashrc && cd #{node['graylog2']['web_path']}/current && RAILS_ENV=production rake subscriptions:send"
 end
 
-# RELOAD APACHE FOR CONFIG CHANGES
 service "apache2" do 
   action :reload
-end
-
-# NOTIFICATION FOR THE FINISHED INSTALLATION
-log "Graylog2 Web-Interface s successfully installed and configured." do
-  action :nothing
 end
