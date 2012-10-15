91d70713c966357ba404dbed256760278c12eabb
Vitaly Gorodetsky <gorodetsky@gmail.com>
2012-10-15 19:08:41 +0200

    version bump to 0.0.23

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name "graylog2"
 maintainer        "Sebastian Wendel, SourceIndex IT-Services"
 maintainer_email  "packages@sourceindex.de"
 license           "Apache 2.0"
 description       "Installs and configures Graylog2"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.22"
+version           "0.0.23"
 recipe            "graylog2", "Installs and configures Graylog2"
 
 %w{apt yum java rvm apache2 mongodb elasticsearch logrotate}.each do |pkg|
