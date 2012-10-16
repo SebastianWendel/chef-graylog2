f29d0623096c108e6c289907390f1f6086be2621
Vitaly Gorodetsky <gorodetsky@gmail.com>
2012-10-16 18:40:28 +0200

    metadata fix and 0.0.24 version bump

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,17 @@ maintainer_email  "packages@sourceindex.de"
 license           "Apache 2.0"
 description       "Installs and configures Graylog2"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.23"
+version           "0.0.24"
 recipe            "graylog2", "Installs and configures Graylog2"
 
-%w{apt yum java rvm apache2 mongodb elasticsearch logrotate}.each do |pkg|
+%w{apt yum java logrotate}.each do |pkg|
   depends pkg
 end
+
+depends 'mongodb','= 0.0.0'
+depends 'rvm', '~> 0.9.1'
+depends 'apache2', '~> 1.2.0'
+depends 'elasticsearch', '= 0.1.0'
 
 %w{redhat centos ubuntu debian}.each do |os|
   supports os
